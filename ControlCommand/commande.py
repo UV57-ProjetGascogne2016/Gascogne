3abf0c757cc48465a64558f900c39476f3a5eec1
Sylvain Hunault <sylvain.hunault@ensta-bretagne.org>
2016-02-09 16:58:20 +0100

    version 0.3 : repulsive coasts, TODO list

--- ControlCommand/commande.py
+++ ControlCommand/commande.py
@@ -4,6 +4,12 @@ import numpy as np
 from pyIbex import *
 from vibes import vibes
 
+#TODO
+#Adapt regulator to always provide a positive u
+#Allow to transform u into PWM
+#Make a function to determine the heading with GPS coordinates
+#Be able to read a file with the coordinates of each ellipsoides and spead at each point and if necessary coasts
+
 class Robot:
     def __init__(self, x, y, V, theta, dt, numero, couleur):
         #Etats
@@ -29,7 +35,11 @@ class Robot:
         return True
         
     def calculConsigne(self):
+        distances = self.distanceCote()
         w = self.Vhat - 2*(self.X - self.Xhat)
+        # Repulsive fiel from the coasts
+        for dist in distances:
+            w = w - 0.01*dist/(np.linalg.norm(dist)**3)
         vbar = np.linalg.norm(w)
         thetabar = math.atan2(w[1],w[0])
         return vbar, thetabar
@@ -40,10 +50,19 @@ class Robot:
         u_v = self.kp[0]*(vbar-self.V)+ np.random.normal(scale=0.1)
         u_theta = self.kp[1]*2*math.atan(math.tan(0.5*(thetabar-self.theta)))+ np.random.normal(scale=0.1)
         
+        # print(u_v)
+        # print(u_theta)
+        
+        # if(u_v>100):
+        #      u_v=100
+        # if(u_theta>1):
+        #     u_theta=1
+        
         self.u = np.array([u_v,u_theta])
   
         return True
         
+    # # # DEBUT PARTIE SIMULATION : EULER # # #
     def subMove(self):
         xp = self.V*math.cos(self.theta)
         yp = self.V*math.sin(self.theta)
@@ -57,9 +76,34 @@ class Robot:
         self.theta = self.theta + self.dt *thetap #+ np.random.normal(scale=0.1)
     
         return True
+    # # # FIN PARTIE SIMULATION # # #
         
-    def getVandPsi(self):
-        return True
+    def distanceCote(self):
+        cotes = np.array([[-4.4,47.8],[-3.8,47.8],[-2.3,47.2],[-2,46.6],[-1.3,44.4],[-2.0,43.3],[-3.6,43.5],[-2.8,43.4]])
+        distances = np.zeros([len(cotes)-1,2]) #to have same array as cotes
+        for i in range(0,len(cotes)-1):
+            distances[i] = dist(cotes[i],cotes[i+1],self.X)
+        return distances
+    
+    
+def dist(A, B, C): # C is the point
+    AB = B-A
+    AC = C-A
+    BC = C-B
+    
+    normAB = np.linalg.norm(AB)
+
+    u =  np.linalg.norm(AC*AB/normAB)
+
+    if u > 1:
+        u = 1
+        return BC
+    elif u < 0:
+        return AC
+    else:
+        res = np.cross(AC,AB)/normAB
+        return res
+    
     
 def frange(start, stop, step):
     # range for floats
@@ -100,11 +144,13 @@ def traj(t,delta):
     dc = np.array([-f*math.sin(f*t+delta), f*math.cos(f*t+delta)])+dTransla
     
     #Recentrer l'ellipse au niveau du golf de gascogne
-    recenter = np.array([-4.0,45])
+    recenter = np.array([-2,44.6])
     w = np.dot(T,c)+recenter
     dw = np.dot(dT,c) + np.dot(T,dc)
 
     return w, dw
+
+
     
 if __name__ == '__main__':
     
@@ -118,10 +164,11 @@ if __name__ == '__main__':
 
     vibes.beginDrawing()
     vibes.clearFigure() 
-    vibes.drawLine([[-4.4,47.8],[-3.8,47.8],[-2.3,47.2],[-2,46.6],[-1.3,47.7],[-1.3,44.4],[-2.0,43.3],[-3.6,43.5],[-2.8,43.4]]) 
-    for t in frange(0,2,h):
-        #time.sleep(0.05)
-        # vibes.clearFigure()
+    vibes.drawLine([[-4.4,47.8],[-3.8,47.8],[-2.3,47.2],[-2,46.6],[-1.3,44.4],[-2.0,43.3],[-3.6,43.5],[-2.8,43.4]])
+    
+    for t in frange(0,100,h):
+        time.sleep(0.1)
+        #vibes.clearFigure()
         time1 = time.time()
         for rob in listRobots:
             #Draw circle of position
