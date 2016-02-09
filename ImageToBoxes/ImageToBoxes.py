cb112e88a45bbbe6fc486316cfba3f5c7d7021f1
Mael LE GALLIC <legal.mael@hotmail.fr>
2016-02-09 14:11:51 +0100

    Modifications on the SIVIA test of the file ImageToBoxes. Add: frame origin and scale. The 4 images problem is resolved by using the image shape as the size of the SIVIA input intervalVector.

--- ImageToBoxes/ImageToBoxes.py
+++ ImageToBoxes/ImageToBoxes.py
@@ -12,8 +12,11 @@ from SIVIA import *
 import cv2
 import numpy as np
 
-i0 = 0
-j0 = 0
+# position of the origine frame pixel
+i0 = 500
+j0 = 100
+pixelScale = 10 # pixel/Km
+epsilon = 10*pixelScale
 
 class ClassTest:
     def __init__(self, imgIntegral):
@@ -21,9 +24,10 @@ class ClassTest:
         
     def toPixels(self, x, y):
         r"""get the value in the matrix imgIntegral at the position x, y in the interval world
-        """
-        i = round(x) + i0
-        j = -round(y) + j0
+        """        
+        i = round(x/pixelScale) + i0
+        j = -round(y/pixelScale) + j0
+        
         return i, j
         
     def calculatBox(self,X):
@@ -33,7 +37,6 @@ class ClassTest:
         Bi, Bj = self.toPixels(X[0].ub(),X[1].ub())
         Di, Dj = self.toPixels(X[0].lb(),X[1].lb())
         
-
         # A + C - B - D       
         num = self.l_imgIntegral[Aj][Ai] + self.l_imgIntegral[Cj][Ci] - self.l_imgIntegral[Bj][Bi] - self.l_imgIntegral[Dj][Di]
         
@@ -66,25 +69,24 @@ class ClassTest:
             return IBOOL.UNK
 
 if __name__ == '__main__':
-    img = cv2.imread('france.jpg',0)
+    img = cv2.imread('Gascogne.png',0)
     retval, img_bin = cv2.threshold(img, 254, 1, cv2.THRESH_BINARY_INV)
     imgIntegral = cv2.integral(img_bin)
     
     
-    cv2.namedWindow('img', cv2.WINDOW_AUTOSIZE )
-    cv2.imshow('img',img)
+#    cv2.namedWindow('img', cv2.WINDOW_AUTOSIZE )
+#    cv2.imshow('img',img)
 
-    
-    
-    X0 = IntervalVector(2, [0, 650])
+    j_max, i_max = imgIntegral.shape    
+    X0 = IntervalVector([[-i0*pixelScale, (-i0+i_max-1)*pixelScale],[(j0-j_max+1)*pixelScale, j0*pixelScale]])
     pdc = ClassTest(imgIntegral)
     
     vibes.beginDrawing()
-    vibes.newFigure('ImageIntegralSIVIA')
+    vibes.newFigure('PavedImageSIVIA')
     vibes.setFigureProperties(dict(x=0, y=10, width=500, height=500))
-    SIVIA(X0, pdc, 10)
+    SIVIA(X0, pdc, epsilon)
     vibes.endDrawing()
     
-    cv2.waitKey(0)
+    #cv2.waitKey(0)
     
     
--- ImageToBoxes/README.md
+++ ImageToBoxes/README.md
@@ -4,11 +4,19 @@ Images To Boxes
 
 
 ------------
-Introduction
+Requirements
 ------------
 
-This folder group work on calculating boxes:
+ - OpenCV 3 library.
+ - PYIBEX library. Available: https://github.com/benEnsta/pyIbex
+ - VIBES. Available: http://enstabretagnerobotics.github.io/VIBES/
 
- - ImageIntegralSIVIA.py pave the image of France to a pavage of France.
+------------
+Description
+------------
 
- 
+ImageToBoxes.py returns the pavage of an input image.
+It uses a SIVIA algorithm defined in SIVIA.py.
+The frame origine, the scale and the precision of the pavage are defined by the variables (i0, j0), pixelScale and epsilon.
+    
+
